0cac075f5f8d872b4f4f4c2f32d122fca31f0855
mrana007 <rana.es.bc@gmail.com>
2023-11-05 03:26:50 +0600

    added footer section

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,10 +1,27 @@
-
+import { FaTwitter, FaYoutube, FaFacebookF, FaInstagram } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
-        <div>
-           <h1>this is</h1> 
-        </div>
+        <footer className="footer footer-center p-10 bg-black text-white rounded">
+  <nav className="grid grid-flow-col gap-4">
+    <a className="link link-hover">About us</a> 
+    <a className="link link-hover">Contact</a> 
+    <a className="link link-hover">Career</a> 
+    <a className="link link-hover">Brand</a>
+  </nav> 
+  <nav>
+    <div className="grid grid-flow-col gap-4">
+    <Link><FaFacebookF className='text-blue-500 text-4xl'></FaFacebookF></Link>
+    <Link><FaInstagram className='text-red-500 text-4xl'></FaInstagram></Link>
+    <Link><FaTwitter className='text-blue-500 text-4xl'></FaTwitter></Link>
+    <Link><FaYoutube className='text-red-500 text-4xl'></FaYoutube></Link>
+    </div>
+  </nav> 
+  <aside>
+    <p>Copyright © 2023 - All right reserved by StudyHive</p>
+  </aside>
+</footer>
     );
 };
 
--- src/components/layout/Navbar/Navbar.jsx
+++ src/components/layout/Navbar/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
         {navLinks}
       </ul>
     </div>
-    <a className="btn btn-ghost normal-case text-xl font-extrabold text-green-700">Group Study</a>
+    <a className="btn btn-ghost normal-case text-xl font-extrabold text-green-700">Study Hive</a>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
